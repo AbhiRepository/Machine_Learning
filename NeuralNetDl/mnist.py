7324cb311d62df77f5b94cafeb9375227b9bfc92
Abhishek Patel <b516002@iiit-bh.ac.in>
2017-07-11 06:33:40 -1200

    Update mnist.py

--- NeuralNetDl/mnist.py
+++ NeuralNetDl/mnist.py
@@ -57,6 +57,6 @@ def train_neural_network(x):
 
 		correct = tf.equal(tf.argmax(prediction,1),tf.argmax(y,1))
 		accuracy = tf.reduce_mean(tf.cast(correct,'float'))
-		print('Accuracy',accuracy.eval({x:mnist.text.image,y:mnist.test.labels}))
+		print('Accuracy',accuracy.eval({x:mnist.test.images,y:mnist.test.labels}))
 
 train_neural_network(x)
