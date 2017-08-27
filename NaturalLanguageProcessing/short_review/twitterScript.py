f4e8a38758373aa1a51cae47ca16e725f224f1a8
Abhishek Patel <b516002@iiit-bh.ac.in>
2017-08-27 08:01:51 -1200

    Add files via upload

---
+++ NaturalLanguageProcessing/short_review/twitterScript.py
@@ -0,0 +1,10 @@
+import sentiment_mod as s
+
+print(s.sentiment("this movie is the best movie i've ever saw till date, keep it up"))
+print(s.sentiment("worst movie ever, just stop making this kind movie for god sake!!"))
+
+
+
+
+
+
